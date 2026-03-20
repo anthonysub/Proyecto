7d8806fa4b462f4c60199b9c6715577c4edc30ea
anthony <anthony.coy@itmes.edu.gt>
2026-03-20 13:29:49 -0600

    correccion de los pytest - anthony

--- app/repositories/usuario_repository.py
+++
@@ -1,9 +0,0 @@
-class UsuarioRepository:
-    def __init__(self):
-        self.usuarios = []
- 
-    def guardar(self, usuario):
-        self.usuarios.append(usuario)
- 
-    def listar(self):
-        return self.usuarios
